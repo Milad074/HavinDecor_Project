cd5fac5489e3c64d4f033c0310aa57ea3fe3aa6e
Milad <Milad@DESKTOP-K4U9SRE>
2021-12-22 17:34:31 +0330

    ProductCategory Management Impalimanted

---
+++ HavinDecor/ShopManagement.Infrastructure.EFCore/Mapping/ProductCategoryMapping.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ShopManagement.Domain.ProductCategoryAgg;
+
+namespace ShopManagement.Infrastructure.EFCore.Mapping
+{
+    public class ProductCategoryMapping : IEntityTypeConfiguration<ProductCategory>
+    {
+        public void Configure(EntityTypeBuilder<ProductCategory> builder)
+        {
+            builder.ToTable("ProductCategories");
+
+            builder.HasKey(pc => pc.Id);
+
+            builder.Property(pc => pc.Name).HasMaxLength(255).IsRequired();
+            builder.Property(pc => pc.Description).HasMaxLength(600);
+            builder.Property(pc => pc.Picture).HasMaxLength(1000);
+            builder.Property(pc => pc.PictureAlt).HasMaxLength(255);
+            builder.Property(pc => pc.PictureTitle).HasMaxLength(500);
+            builder.Property(pc => pc.Keywords).HasMaxLength(80).IsRequired();
+            builder.Property(pc => pc.MetaDescription).HasMaxLength(150).IsRequired();
+            builder.Property(pc => pc.Slug).HasMaxLength(300).IsRequired();
+        }
+    }
+}
---
+++ HavinDecor/ShopManagement.Infrastructure.EFCore/Repository/ProductCategoryRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using _0_Framework.Infrastructure;
+using ShopManagement.Application.Contracts.ProductCategory;
+using ShopManagement.Domain.ProductCategoryAgg;
+
+namespace ShopManagement.Infrastructure.EFCore.Repository
+{
+    public class ProductCategoryRepository : RepositoryBase<long , ProductCategory> , IProductCategoryRepository
+    {
+        private readonly ShopContext _context;
+
+        public ProductCategoryRepository(ShopContext context) :base(context)
+        {
+            _context = context;
+        }
+
+        public EditProductCategory GetDetails(long id)
+        {
+            return _context.ProductCategories.Select(pc => new EditProductCategory()
+            {
+                Id = pc.Id,
+                Name = pc.Name,
+                Description = pc.Description,
+                Picture = pc.Picture,
+                PictureAlt = pc.PictureAlt,
+                PictureTitle = pc.PictureTitle,
+                Keywords = pc.Keywords,
+                MetaDescription = pc.MetaDescription,
+                Slug = pc.Slug
+            }).FirstOrDefault(pc => pc.Id == id);
+        }
+
+        public List<ProductCategoryViewModel> Search(ProductCategorySearchModel searchModel)
+        {
+            var query = _context.ProductCategories.Select(pc => new ProductCategoryViewModel()
+            {
+                Id = pc.Id,
+                Name = pc.Name,
+                Picture = pc.Picture,
+                CreationDate = pc.CreationDate.ToString()
+            });
+
+            if (!string.IsNullOrWhiteSpace(searchModel.Name))
+            {
+                query = query.Where(pc => pc.Name.Contains(searchModel.Name));
+            }
+
+            return query.OrderByDescending(pc => pc.Id).ToList();
+        }
+
+        
+    }
+}
